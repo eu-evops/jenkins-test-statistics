98c5d38d7a875e5f744e4362230ee836e7f60e9c
Stanislaw Wozniak @ BA <stanislaw.wozniak@ba.com>
2016-01-21 12:46:27 +0000

    Fixing path to the dist folder

--- app/web.js
+++ app/web.js
@@ -2,5 +2,5 @@ var express = require('express');
 var gzippo = require('gzippo');
 var app = express();
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/../dist"));
 app.listen(process.env.PORT || 9000);
